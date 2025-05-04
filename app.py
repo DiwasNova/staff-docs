f0beaaf20a4eb0e2bc34c302fafc85a268a7b37b
DiwasNova <diwas@novacareaustralia.com.au>
2025-05-04 10:16:23 +1000

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,8 @@ def upload_file():
     return render_template('upload.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
